c6fb1a9fbc4a1c906d41f1d152718b7aadbf5a88
knavekaiser <naeem.ahmad.9m@gmail.com>
2020-06-10 23:56:29 +0600

    final

--- ui.js
+++ ui.js
@@ -30,7 +30,7 @@ function resizeWindow() {
   document
     .querySelectorAll(".tbody")
     .forEach(
-      (tbody) => (tbody.style.height = `${innerContainer.clientHeight - 96}px`)
+      (tbody) => (tbody.style.height = `${innerContainer.clientHeight - 94}px`)
     );
 }
 
